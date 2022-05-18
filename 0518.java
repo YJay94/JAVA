08d9e04f7b13319391a8cca034749833f6004745
YJay94 <83999253+YJay94@users.noreply.github.com>
2022-05-18 18:03:31 +0900

    Add files via upload

---
+++ 0518.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫번째 숫자를 입력하세요:");
+        int first = sc.nextInt();
+        System.out.print("두번째 숫자를 입력하세요:");
+        int second = sc.nextInt();
+
+        int result = first + second;
+        System.out.printf("%d + %d = %d 입니다. %n", first, second, result);
+    }
+}
